e2c30736e25e315ed9a1cfb578f70ac4ab8130f7
PedroOliveira2003 <125292368+PedroOliveira2003@users.noreply.github.com>
2023-06-26 21:47:54 -0300

    Inserir Curso

--- VIEW/curso/lstcurso.php
+++ VIEW/curso/lstcurso.php
@@ -1,10 +1,12 @@
 <?php
 
-    include_once '../../DAL/conexao.php';
-    use DAL\Conexao;
-    $sql = "select * from curso;";
-    $con = Conexao::conectar();
-    $lstcurso = $con->query($sql);
+include_once '../../DAL/conexao.php';
+
+use DAL\Conexao;
+
+$sql = "select * from curso;";
+$con = Conexao::conectar();
+$lstcurso = $con->query($sql);
 ?>
 
 
@@ -14,11 +16,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <title>Listar Curso</title>
 </head>
@@ -31,7 +31,7 @@
             <th>ID</th>
             <th>LETRA</th>
             <th>VALOR</th>
-            
+
         </tr>
 
         <?php
